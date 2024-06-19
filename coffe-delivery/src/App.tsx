19380569f8ffd9a4e9fc1382848fb5d36c58e8c9
Albert Allan <albertallanramos@gmail.com>
2024-06-19 00:13:13 -0300

    Criação do projeto e padronização de cores no tailwind

---
+++ coffe-delivery/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="bg-coffe_yellow_dark">COFFE</h1>
+    </>
+  )
+}
+
+export default App
---
+++ coffe-delivery/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      coffe_yellow_dark: '#C47f17',
+      coffe_yellow: '#DBAC2c',
+      coffe_yellow_light: '#F1e9c9',
+      coffe_purple_dark: '#4b2995',
+      coffe_purple: '#8047F8',
+      coffe_purple_light: '#Ebe5f9',
+      coffe_base_title: '#272221',
+      coffe_base_subtitle: '#403937',
+      coffe_base_text: '#574F4d',
+      coffe_base_label: '#8d8686',
+      coffe_base_hover: '#D7D5D5',
+      coffe_base_button: '#e6e5e5',
+      coffe_base_input: '#Ededed',
+      coffe_base_card: '#F3F2f2',
+      coffe_background: '#FAFAFA',
+      coffe_white: '#ffffff',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
